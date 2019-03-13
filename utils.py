9b7af5827ac4745556ccddbadd6ed6f203d7f5e4
Martin Kondor <martinkondor@gmail.com>
2019-03-13 10:24:46 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,3 +21,11 @@ def sumcols(df):
 
     return X
     
+    
+def get_regr_coeffs(X, y):
+    n = X.shape[0]
+    p = n*(X**2).sum() - X.sum()**2
+    a = ( n*(X*y).sum() - X.sum()*y.sum() ) / p
+    b = ( y.sum()*(X**2).sum() - X.sum()*(X*y).sum() ) / p
+    return a,b
+    
